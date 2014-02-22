bbb46ebf0f3b985706a125336e3b3084e79b3dcd
Oleksii Svarychevskyi <oleksii.svarychevskyi@sitewards.com>
2014-02-22 11:13:33 +0100

    Added extension skeleton

---
+++ app/code/community/Hackathon/Gamification/Helper/Data.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category    Hackathon
+ * @package     Hackathon_Gamification
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @generator   http://www.mgt-commerce.com/kickstarter/ Mgt Kickstarter
+ */
+
+class Hackathon_Gamification_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
