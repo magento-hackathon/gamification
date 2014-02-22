4f034970383a775a57d2f8f59e260c069b12f3fd
Oleksii Svarychevskyi <oleksii.svarychevskyi@sitewards.com>
2014-02-22 13:27:10 +0100

    Created Rule interface to provide access to defined events and theirs config fields

---
+++ app/code/community/Hackathon/Gamification/Helper/Rule.php
@@ -0,0 +1,13 @@
+<?php
+class Hackathon_Gamification_Helper_Rule extends Mage_Core_Helper_Abstract
+{
+    public function getEventsForForm()
+    {
+        //@TODO return event array for form dropdown
+    }
+
+    public function getEventConfig($sEventName)
+    {
+        //@TODO return event config fields
+    }
+}
